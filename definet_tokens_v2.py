b1a97bb774543a7c33ffe3aa11030a63cbc1f8c0
vitidm <victordm97@gmail.com>
2023-02-10 10:44:20 +0100

    Add files via upload

--- definet_tokens_v2.py
+++ definet_tokens_v2.py
@@ -708,7 +708,7 @@ async def main():
         
 
         print(f"Finish loop at: {datetime.now().strftime('%d/%m/%Y %H:%M:00')}")
-        time.sleep(2000)   
+        time.sleep(1000)   
         sqlUpdatePostTelegramTokenInfo("post_telegram_token_info")  
         
         sqlUpdateTrendingTokens("trending_tokens")
